77c2905570d88896cff74996d59ce85d05ac9549
Pratik Patil <patil.pratiq@gmail.com>
2023-04-06 16:13:51 -0400

    inital site

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="Calculator-header">
-        <h1 className="Calculator-title">My Personal Portfolio</h1>
-        <p className="Calculator-text">Welcome to my calculator-themed portfolio website!</p>
+      <header className="HarryPotter-header">
+        <h1 className="HarryPotter-title">My Personal Portfolio</h1>
+        <p className="HarryPotter-text">Welcome to my Harry Potter-themed portfolio website!</p>
       </header>
-      <section className="Calculator-section">
+      <section className="HarryPotter-section">
         <h2>About Me</h2>
-        <p className="Calculator-text">Insert your bio and information about your skills and experiences here.</p>
+        <p className="HarryPotter-text">Insert your bio and information about your skills and experiences here.</p>
       </section>
-      <section className="Calculator-section">
+      <section className="HarryPotter-section">
         <h2>My Work</h2>
-        <p className="Calculator-text">Insert examples of your work and projects here.</p>
+        <p className="HarryPotter-text">Insert examples of your work and projects here.</p>
       </section>
-      <section className="Calculator-section">
+      <section className="HarryPotter-section">
         <h2>Contact Me</h2>
-        <p className="Calculator-text">Insert your contact information here.</p>
+        <p className="HarryPotter-text">Insert your contact information here.</p>
       </section>
-      <footer className="Calculator-footer">
-        <p className="Calculator-text">© Your Name | All Rights Reserved</p>
+      <footer className="HarryPotter-footer">
+        <p className="HarryPotter-text">© Your Name | All Rights Reserved</p>
       </footer>
     </div>
   );
