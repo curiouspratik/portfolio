7dcd16a1a867f5f9e3a208f7b460cd032080d392
Pratik Patil <patil.pratiq@gmail.com>
2023-04-06 15:47:22 -0400

    inital site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Calculator-header">
+        <h1 className="Calculator-title">My Personal Portfolio</h1>
+        <p className="Calculator-text">Welcome to my calculator-themed portfolio website!</p>
       </header>
+      <section className="Calculator-section">
+        <h2>About Me</h2>
+        <p className="Calculator-text">Insert your bio and information about your skills and experiences here.</p>
+      </section>
+      <section className="Calculator-section">
+        <h2>My Work</h2>
+        <p className="Calculator-text">Insert examples of your work and projects here.</p>
+      </section>
+      <section className="Calculator-section">
+        <h2>Contact Me</h2>
+        <p className="Calculator-text">Insert your contact information here.</p>
+      </section>
+      <footer className="Calculator-footer">
+        <p className="Calculator-text">© Your Name | All Rights Reserved</p>
+      </footer>
     </div>
   );
 }
